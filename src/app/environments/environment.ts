b8762799ac95238d5a8dffdf6a656e5a0b3a3067
jonathan_escuela2 <jj.garcia.dl.1994@gmail.com>
2024-09-23 18:31:18 -0700

    add production env

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
    /*  apiURL: 'http://localhost:3000', */
     apiURL: 'https://hmm-back-production.up.railway.app',
     /* domain: 'localhost:3000', */
-    domain: 'hmm-back-production.up.railway.app'
+    domain: 'hmm-back-production.up.railway.app',
+    production: true
 };
